ceedf6fc3717802410dcbc32113462c71b5ff1d1
Camila <cameg0329@gmail.com>
2024-02-19 13:02:39 -0300

    init

--- app.js
+++ app.js
@@ -4,17 +4,33 @@ const cors = require("cors");
 const sequelize = require("./database/db");
 const app = express();
 require("./database/associate");
-
+//require("./database/seed");
 app.use(cors()); //Habilitamos peticiones (GET - PUT - POST - DELETE)
 app.use(logger("dev")); //Muestra peticiones
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // ?
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end("Hello! Go to item:");
+});
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end("Item:");
+});
 
 //Defino las rutas
-
+const port = process.env.PORT || 3000;
 app.use("/", require("./routes"));
 
-app.listen(5000, () => {
+app.listen(port, () => {
   sequelize
     .sync({ force: false })
     .then(() => {
@@ -23,5 +39,6 @@ app.listen(5000, () => {
     .catch((error) => {
       console.log("Se ha producido un error", error);
     });
+    
   console.log("Servidor iniciado en el puerto 5000");
 });
