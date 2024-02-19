4cd5a41e94f667e8725f566fdb120ac415a1fac1
Camila <cameg0329@gmail.com>
2024-02-19 10:00:04 -0300

    init

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("pw_1", "root", "toor", {
-  host: "127.0.0.1",
+/* 'database', 'username', 'password' */
+const sequelize = new Sequelize("railway", "root", "fb1F32DcC3Cbdcf-gb62ge-GH1F1d3ed", {
+  host: "monorail.proxy.rlwy.net",
   dialect: "mysql",
+  port: 14222,
   operatorsAliases: 0,
 });
 
